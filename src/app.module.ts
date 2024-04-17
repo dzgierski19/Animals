dc40a422f3d9faea12f85dbd6179aff8dc604021
dzgierski19 <dzgierskigit@gmail.com>
2024-04-17 12:34:49 +0200

    added import for AnimalsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AnimalsModule } from './animals/animals.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AnimalsModule],
 })
 export class AppModule {}
