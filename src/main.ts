bbd3266a3c858a29898c6c916b0630fbadebf987
dzgierski19 <dzgierskigit@gmail.com>
2024-04-19 22:38:34 +0200

    dotenv at the top

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 console.log(process.env);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
 }
+
 bootstrap();
