d322903025d4e83b2da20ff7e74935df739953b7
dzgierski19 <dzgierskigit@gmail.com>
2024-05-02 18:59:26 +0200

    controller methods

--- src/animals/animals.controller.ts
+++ src/animals/animals.controller.ts
@@ -1,27 +1,87 @@
-import { Controller, Delete, Get, Param, Patch, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  HttpCode,
+  NotFoundException,
+} from '@nestjs/common';
 import { AnimalsService } from './animals.service';
-import { AnimalToCreate } from '../../db/types';
+import { AnimalType } from '../../db/types';
+import { ZodValidationPipe } from './schemas/ZodValidationPipe';
+import {
+  CreateAnimalDto,
+  CreateAnimalsDto,
+  UpdateAnimalDto,
+  createAnimalSchema,
+  createAnimalsSchema,
+  updateAnimalSchema,
+} from './schemas/schemas';
 
 @Controller('animals')
 export class AnimalsController {
   constructor(private readonly animalsService: AnimalsService) {}
+
   @Get()
-  getAnimals() {
-    return this.animalsService.getAllAnimals();
+  async getAll() {
+    const animals = await this.animalsService.getAll();
+    return animals;
   }
-  @Get(':id')
-  getOne(@Param('id') id: string) {
-    return this.animalsService.getAnimal(id);
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAnimalSchema))
+  async addOne(@Body() animal: CreateAnimalDto) {
+    console.log(animal);
+    await this.animalsService.addOne(animal);
   }
-  @Delete(':id')
-  deleteOne(@Param('id') id: string) {
-    return this.animalsService.deleteAnimal(id);
+
+  @Post('add-few')
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAnimalsSchema))
+  async addMoreThanOne(@Body() animals: CreateAnimalsDto) {
+    console.log(animals);
+    await this.animalsService.addMoreThanOne(animals);
   }
-  @Patch(':id')
-  update(
-    @Param(':id') id: string,
-    @Query('data') data: Partial<AnimalToCreate>,
+
+  @Post(':type')
+  @HttpCode(201)
+  async addByType(
+    @Param('type') type: AnimalType,
+    @Body() animals: CreateAnimalDto[],
   ) {
-    return this.animalsService.updateAnimal(id, data);
+    const filteredType = animals.filter((element) => element.type === type);
+    console.log(filteredType);
+    if (!filteredType)
+      throw new NotFoundException(`User with ${type} not found`);
+    await this.animalsService.addMoreThanOne(filteredType);
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: string) {
+    const animal = await this.animalsService.getOne(id);
+    return animal;
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  async deleteOne(@Param('id') id: string) {
+    await this.animalsService.deleteOne(id);
+    return { id };
+  }
+
+  @Patch(':id')
+  @HttpCode(204)
+  async updateOne(
+    @Param('id') id: string,
+    @Body(new ZodValidationPipe(updateAnimalSchema)) data: UpdateAnimalDto,
+  ) {
+    await this.animalsService.updateOne(id, data);
+    const animal = await this.animalsService.getOne(id);
+    return animal;
   }
 }
