b136dece8cbd5a17e814bcdc3da905acc733ad45
dzgierski19 <dzgierskigit@gmail.com>
2024-05-02 18:58:32 +0200

    updated module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AnimalsModule } from './animals/animals.module';
+import { mockDatabaseService } from './database-mock/database-mock.service';
 
 @Module({
   imports: [AnimalsModule],
+  providers: [mockDatabaseService],
 })
 export class AppModule {}
