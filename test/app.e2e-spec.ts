87264b712cc8d257b224ed08c67c544cc77cea10
dzgierski19 <dzgierskigit@gmail.com>
2024-05-08 20:07:58 +0200

    tests for post and get for /animals endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import {
+  DatabaseAdapter,
+  IDatabaseAdapter,
+} from './../src/database/database.service';
+import {
+  AnimalsService,
+  IAnimalsService,
+} from './../src/animals/animals.service';
+import { AnimalsController } from './../src/animals/animals.controller';
+import { AppModule } from './../src/app.module';
+import { ANIMALTYPE, Animal, AnimalToCreate } from 'db/types';
+import knex, { Knex } from 'knex';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let knexInstance: Knex;
+  let animalsService = { getAll: () => ['test'] };
+  let animalsServ: IAnimalsService;
+  let animalsController: AnimalsController;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
-
+    })
+      // .overrideProvider(DatabaseAdapter)
+      // .useValue(TestDatabaseAdapterService)
+      .compile();
+    process.env.NODE_ENV = 'testing';
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    // testDatabaseAdapter = new TestDatabaseAdapterService();
+    // animalsServ = new AnimalsService(testDatabaseAdapter);
   });
 
-  it('/ (GET)', () => {
+  it('/GET animals', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/animals')
+      .expect(HttpStatus.OK)
+      .expect([]);
+    // .expect(fakeAnimal);l;
+  });
+
+  it('/POST animals', () => {
+    const newAnimal: AnimalToCreate = {
+      name: 'FAKE_ANIMAL',
+      type: ANIMALTYPE.BIRD,
+    };
+    return request(app.getHttpServer())
+      .post('/animals')
+      .send(newAnimal)
+      .expect(HttpStatus.CREATED);
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
+
+// how to clear db
