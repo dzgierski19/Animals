0d54d31c9b4ed160e966245f4ac2a4426afef1f5
dzgierski19 <dzgierskigit@gmail.com>
2024-05-08 19:39:53 +0200

    deleted unnecessary console.log

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-console.log(process.env);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
