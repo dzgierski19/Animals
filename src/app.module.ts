6e769db907c895c9b66b340b65c137a71504b56c
dzgierski19 <dzgierskigit@gmail.com>
2024-06-03 21:41:33 +0200

    deleted logger (testing middleware)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 
 import { AnimalsModule } from './animals/animals.module';
-import { LoggerMiddleware } from './middleware/pagination.middleware';
+// import { LoggerMiddleware } from './middleware/pagination.middleware';
 
 @Module({
   imports: [AnimalsModule],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: 'animals', method: RequestMethod.GET });
-  }
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(LoggerMiddleware)
+  //     .forRoutes({ path: 'animals', method: RequestMethod.GET });
+  // }
 }
