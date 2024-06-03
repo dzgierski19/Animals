0c103d412e94473cb30c85bb0249684ae6ce3e7e
dzgierski19 <dzgierskigit@gmail.com>
2024-06-03 15:27:20 +0200

    updated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 
 import { AnimalsModule } from './animals/animals.module';
-import { DatabaseAdapter } from './database/database.service';
-import {
-  LoggerMiddleware,
-  pagination,
-} from './middleware/pagination.middleware';
-import { AnimalsController } from './animals/animals.controller';
-import { AnimalsService } from './animals/animals.service';
+import { LoggerMiddleware } from './middleware/pagination.middleware';
 
 @Module({
-  providers: [AnimalsModule],
-  // controllers: [AnimalsController],
-  // providers: [DatabaseAdapter, AnimalsService],
+  imports: [AnimalsModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
