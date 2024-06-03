ac24bc80ea3ac9b6f50e6060a6b466c120561426
dzgierski19 <dzgierskigit@gmail.com>
2024-06-03 21:45:43 +0200

    deleted commented code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,8 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { AnimalsModule } from './animals/animals.module';
-// import { LoggerMiddleware } from './middleware/pagination.middleware';
 
 @Module({
   imports: [AnimalsModule],
 })
-export class AppModule {
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(LoggerMiddleware)
-  //     .forRoutes({ path: 'animals', method: RequestMethod.GET });
-  // }
-}
+export class AppModule {}
