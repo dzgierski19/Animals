e6ddac985aefe11c19aef2d36b52670e9a8b988a
dzgierski19 <dzgierskigit@gmail.com>
2024-06-03 21:41:06 +0200

    pagination middleware refactor

--- src/middleware/pagination.middleware.ts
+++ src/middleware/pagination.middleware.ts
@@ -4,13 +4,13 @@ import { Request, Response, NextFunction } from 'express';
 type paginate = Record<'next' | 'previous', { page: number; limit: number }>;
 type result<T> = paginate & Record<'results', T[]>;
 
-@Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
-    next();
-  }
-}
+// @Injectable()
+// export class LoggerMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+//     console.log('Request...');
+//     next();
+//   }
+// }
 
 // @Injectable()
 // export class PaginationMiddleware implements NestMiddleware {
@@ -41,33 +41,31 @@ export class LoggerMiddleware implements NestMiddleware {
 //     next();
 //   }
 // }
-export function pagination<T>(model: T[]) {
+
+export function pagination<T>(model: T[], limit: number = 5, page: number = 1) {
   return (req: Request, res: Response, next: NextFunction) => {
-    console.log(req);
-    const { page, limit } = req.query;
-    console.log(req.query);
-    if (limit && page) {
-      const limitToNumber = +limit;
-      const pageToNumber = +page;
-      const startIndex = (pageToNumber - 1) * limitToNumber;
-      const endIndex = pageToNumber * limitToNumber;
-      const result = {} as result<T>;
-      if (endIndex < model.length) {
-        result.next = {
-          page: pageToNumber + 1,
-          limit: limitToNumber,
-        };
-      }
-      if (startIndex > 0) {
-        result.previous = {
-          page: pageToNumber - 1,
-          limit: limitToNumber,
-        };
-      }
-      result.results = model.slice(startIndex, endIndex);
-      console.log(result);
-      req.body = result;
+    // console.log(req);
+    // const { page, limit } = req.query;
+    // console.log(req.query);
+    // const limitToNumber = +limit || 5;
+    // const pageToNumber = +page || 1;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const result = {} as result<T>;
+    if (endIndex < model.length) {
+      result.next = {
+        page: page + 1,
+        limit: limit,
+      };
     }
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    result.results = model.slice(startIndex, endIndex);
+    req.body = result;
     next();
   };
 }
