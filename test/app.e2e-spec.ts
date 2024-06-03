a9ea5b7f4868006c8388d10d61526fa381517026
dzgierski19 <dzgierskigit@gmail.com>
2024-06-03 15:38:21 +0200

    added logic to accept only testing NODE_ENV

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,62 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import {
-  DatabaseAdapter,
-  IDatabaseAdapter,
-} from './../src/database/database.service';
-import {
-  AnimalsService,
-  IAnimalsService,
-} from './../src/animals/animals.service';
-import { AnimalsController } from './../src/animals/animals.controller';
 import { AppModule } from './../src/app.module';
-import { ANIMALTYPE, Animal, AnimalToCreate } from 'db/types';
-import knex, { Knex } from 'knex';
+import { ANIMALTYPE } from './../db/types';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let knexInstance: Knex;
-  let animalsService = { getAll: () => ['test'] };
-  let animalsServ: IAnimalsService;
-  let animalsController: AnimalsController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      // .overrideProvider(DatabaseAdapter)
-      // .useValue(TestDatabaseAdapterService)
-      .compile();
-    process.env.NODE_ENV = 'testing';
+    }).compile();
     app = moduleFixture.createNestApplication();
     await app.init();
-
-    // testDatabaseAdapter = new TestDatabaseAdapterService();
-    // animalsServ = new AnimalsService(testDatabaseAdapter);
+    if (process.env.NODE_ENV !== 'testing') {
+      throw new Error('Please change your environment to "testing"');
+    }
   });
 
-  it('/GET animals', () => {
-    return request(app.getHttpServer())
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+      console.log('app closed...');
+    } else console.log('app is not closed');
+  });
+
+  it('/GET animals', async () => {
+    return await request(app.getHttpServer())
       .get('/animals')
-      .expect(HttpStatus.OK)
-      .expect([]);
-    // .expect(fakeAnimal);l;
+      .expect(HttpStatus.OK);
   });
 
-  it('/POST animals', () => {
-    const newAnimal: AnimalToCreate = {
-      name: 'FAKE_ANIMAL',
-      type: ANIMALTYPE.BIRD,
-    };
-    return request(app.getHttpServer())
+  it('/POST animal', async () => {
+    return await request(app.getHttpServer())
       .post('/animals')
-      .send(newAnimal)
+      .send({ name: 'FAKE_ANIMAL', type: ANIMALTYPE.BIRD })
       .expect(HttpStatus.CREATED);
   });
+  it('/UPDATE animal', async () => {
+    const response = await request(app.getHttpServer()).get('/animals');
+    const id = response.body.at(-1).id;
+    return await request(app.getHttpServer())
+      .patch(`/animals/${id}`)
+      .send({ name: 'UPDATED_FAKE_ANIMAL' })
+      .expect(204);
+  });
 
-  afterEach(async () => {
-    await app.close();
+  it('/DELETE animal', async () => {
+    const response = await request(app.getHttpServer()).get('/animals');
+    const id = response.body.at(-1).id;
+    return await request(app.getHttpServer())
+      .delete(`/animals/${id}`)
+      .expect(204);
   });
 });
 
