b0239f3006856feb7d21f7622f3cf677026654e4
dzgierski19 <dzgierskigit@gmail.com>
2024-06-03 15:30:29 +0200

    changed migration to add primary

--- db/migrations/20240505160456_update_id_as_primary_key.ts
+++ db/migrations/20240505160456_update_id_as_primary_key.ts
@@ -3,13 +3,26 @@ import type { Knex } from 'knex';
 const tableName = 'animals';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "pgcrypto"');
+
   await knex.schema.alterTable(tableName, function (table) {
-    table.string('id').primary().alter();
+    table.uuid('id').defaultTo(knex.raw('gen_random_uuid()')).notNullable();
+  });
+  await knex.schema.alterTable(tableName, function (table) {
+    table.dropColumn('id');
+  });
+
+  await knex.schema.alterTable(tableName, function (table) {
+    table.renameColumn('new_id', 'id');
+  });
+  await knex.schema.alterTable(tableName, function (table) {
+    table.primary(['id']);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable(tableName, function (table) {
     table.dropPrimary();
+    table.dropColumn('id');
   });
 }
