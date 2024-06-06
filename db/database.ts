65cf7a2b7b6b5edea92a3826b1cb44a2a02e03c1
dzgierski19 <dzgierskigit@gmail.com>
2024-06-06 18:44:56 +0200

    created function for closing knex connection

--- db/database.ts
+++ db/database.ts
@@ -3,3 +3,9 @@ import config from './../knexfile';
 
 export const developmentDb = knex(config.development);
 export const testDb = knex(config.testing);
+
+export const closeKnex = () => {
+  if (process.env.NODE_ENV === 'testing') {
+    testDb.destroy();
+  }
+};
